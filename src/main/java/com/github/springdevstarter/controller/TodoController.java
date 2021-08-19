42ad59078277500e8b74f9595b3765ac7f3fa7f4
abhik <avik5324@gmail.com>
2021-08-19 23:21:15 +0530

    added docker-compose.yml

--- src/main/java/com/github/springdevstarter/controller/TodoController.java
+++ src/main/java/com/github/springdevstarter/controller/TodoController.java
@@ -25,7 +25,7 @@ public class TodoController {
         try {
             todoService.addTodo(newtodo);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
         }
         return "redirect:/";
     }
