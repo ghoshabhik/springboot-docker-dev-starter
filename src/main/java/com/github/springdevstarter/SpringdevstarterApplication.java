7f5d0514b144fa0ca336c3ce031fcbf7b93c01ed
abhik <avik5324@gmail.com>
2021-08-19 18:40:47 +0530

    initial commit

---
+++ src/main/java/com/github/springdevstarter/SpringdevstarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.springdevstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdevstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdevstarterApplication.class, args);
+	}
+
+}
